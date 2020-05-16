696b408ecbe3501b0cb5d273cdc333cc3d345e98
Ephraim Ilunga <ephreycleophace@gmail.com>
2020-05-16 08:41:01 +0200

    add custom colors, routes, screen and complete the welcome screen design

---
+++ mobile/flutter/travel_destinations/lib/constants/custom_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BaseColors {
+  static const Color background = Color(0xFFE9F2FA);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color main = Color(0xFF01ADBE);
+  static const Color darkBlack = Color(0xFF000000);
+  static const Color lightBlack = Color(0xFFA8BABE);
+  static const Color textBlack = Color(0xFF465052);
+  static const Color favorite = Color(0xFFE81765);
+}
