33d7035a7680273dc810572481db5c3eaafa0d0e
Ephraim Ilunga <ephreycleophace@gmail.com>
2021-04-02 22:27:34 +0200

    complete project

--- mobile/flutter/courses/appbrewery/xylophone/android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package co.appbrewery.xylophone;
-
-import io.flutter.embedding.android.FlutterActivity;
-//import android.os.Bundle;
-//import io.flutter.app.FlutterActivity;
-//import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterActivity {}
--- mobile/flutter/courses/appbrewery/xylophone/lib/main.dart
+++ mobile/flutter/courses/appbrewery/xylophone/lib/main.dart
@@ -1,16 +1,42 @@
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  static AudioCache player = AudioCache();
+
+  Widget _buildNoteButton(int noteNumber, Color color) {
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+        child: null,
+        onPressed: () => player.play('note$noteNumber.wav'),
+      ),
+    );
+  }
+
+  Widget _noteButtons() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _buildNoteButton(1, Colors.red),
+        _buildNoteButton(2, Colors.orange),
+        _buildNoteButton(3, Colors.teal),
+        _buildNoteButton(4, Colors.blue),
+        _buildNoteButton(5, Colors.amber),
+        _buildNoteButton(6, Colors.pink),
+        _buildNoteButton(7, Colors.tealAccent),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(child: Text(adjectives.last)),
-        ),
+        backgroundColor: Colors.black,
+        body: SafeArea(child: _noteButtons()),
       ),
     );
   }
