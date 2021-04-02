59db716c3bac787a1fbacf264f75ad81154d4eac
Ephraim Ilunga <ephreycleophace@gmail.com>
2021-04-02 14:16:45 +0200

    first commit

---
+++ mobile/flutter/courses/appbrewery/xylophone/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(child: Text(adjectives.last)),
+        ),
+      ),
+    );
+  }
+}
